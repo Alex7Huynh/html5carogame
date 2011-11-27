db3f822d96810bcef1f1368bb6768e57d58b7e0c
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@aadc8ff0-68f9-87c0-f34d-747eac97cfdd>
2011-11-27 07:31:38 +0000

    CaroServer.

---
+++ CaroServer/CaroServer/Step.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaroServer
+{
+    public class Step
+    {
+        public char CurrentPlayer;
+        public Position p;
+        public Step()
+        {
+            p = new Position(-1, -1);
+            CurrentPlayer = ' ';
+        }
+        public Step(Position pp, char cc)
+        {
+            CurrentPlayer = cc;
+            p = new Position();
+            p.Set(pp);
+        }
+    }
+}
